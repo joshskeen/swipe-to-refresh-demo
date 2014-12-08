1c21ad6b1174ca0ab5c31091838ec30730c3e989
Josh Skeen <josh@joshskeen.com>
2014-12-08 11:40:05 -0500

    updated to better variable names

--- app/src/main/java/com/joshskeen/myapplication/MainActivity.java
+++ app/src/main/java/com/joshskeen/myapplication/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,18 +18,17 @@ public class MainActivity extends Activity {
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private ListView mListView;
     private ArrayAdapter<String> mAdapter;
-    private List<String> mFakeTweets;
+    private List<String> mCatNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
-        mSwipeRefreshLayout.setColorSchemeColors();
-        mListView = (ListView) findViewById(R.id.activity_main_listview);
-        mFakeTweets = Arrays.asList(getResources().getStringArray(R.array.fake_tweets));
 
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mFakeTweets);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+        mListView = (ListView) findViewById(R.id.activity_main_listview);
+        mCatNames = Arrays.asList(getResources().getStringArray(R.array.cat_names));
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mCatNames);
         mListView.setAdapter(mAdapter);
         mSwipeRefreshLayout.setColorSchemeResources(R.color.orange, R.color.green, R.color.blue);
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -39,7 +37,7 @@ public class MainActivity extends Activity {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        mAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, getNewTweets());
+                        mAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, getNewCatNames());
                         mListView.setAdapter(mAdapter);
                         mSwipeRefreshLayout.setRefreshing(false);
                     }
@@ -48,13 +46,13 @@ public class MainActivity extends Activity {
         });
     }
 
-    private List<String> getNewTweets() {
-        List<String> newTweets = new ArrayList<String>();
-        for (int i = 0; i < mFakeTweets.size(); i++) {
-            int randomTweetIndex = new Random().nextInt(mFakeTweets.size() - 1);
-            newTweets.add(mFakeTweets.get(randomTweetIndex));
+    private List<String> getNewCatNames() {
+        List<String> newCatNames = new ArrayList<String>();
+        for (int i = 0; i < mCatNames.size(); i++) {
+            int randomCatNameIndex = new Random().nextInt(mCatNames.size() - 1);
+            newCatNames.add(mCatNames.get(randomCatNameIndex));
         }
-        return newTweets;
+        return newCatNames;
     }
 
 }
